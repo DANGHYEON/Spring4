4b5fb0e9a7ac7a9e12607cb7f9cc6fd6a27203ad
DANGHYEON <rlaalsrl3023@naver.com>
2021-09-07 09:33:40 +0900

    Update QnaService.java

--- src/main/java/com/iu/s4/board/qna/QnaService.java
+++ src/main/java/com/iu/s4/board/qna/QnaService.java
@@ -23,6 +23,8 @@ public class QnaService implements BoardService {
 	@Override
 	public List<BoardDTO> getList(Pager pager) throws Exception {
 		// TODO Auto-generated method stub
+		pager.makeNum(qnaDAO.getCount(pager));
+		pager.makeRow();
 		return qnaDAO.getList(pager);
 	}
 
